c739f1fbf88605f6149e8dd41000ffbd47580fcd
Curtis <curtis99877@gmail.com>
2015-11-24 18:38:47 -0500

    Authentication

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var User = mongoose.model('User');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Gift It' });
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login');
+  if (req.user) res.redirect('/list');
+  else res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err,user) {
+    if(user) {
+      req.logIn(user, function(err) {
+        res.redirect('/');
+      });
+    } else {
+      res.render('login', {message:'Your login or password is incorrect.'});
+    }          
+  })(req, res, next);
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register');
+  if (req.user) res.redirect('/list');
+  else res.render('register');
+});
+
+router.post('/register', function(req, res, next) {
+  User.register(new User({username:req.body.username}), req.body.password, function(err, user){
+    if (err) {
+      res.render('register',{message:'Your registration information is not valid'});
+    } else {
+      passport.authenticate('local')(req, res, function() {
+        res.redirect('/');
+      });
+    }      
+  });
+});
+
+router.get('/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
 });
 
 router.get('/settings', function(req, res, next) {
