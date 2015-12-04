75442f54a386fdb7e27e56120becef41b54b8880
Curtis <curtis99877@gmail.com>
2015-12-03 23:15:12 -0500

    DB update

--- db/db.js
+++ db/db.js
@@ -6,4 +6,4 @@ require('./user');
 require('./friend');
 require('./tag');
 
-mongoose.connect('mongodb://localhost/project');
+mongoose.connect('mongodb://curtis:curtis@ds063134.mongolab.com:63134/heroku_qvj3wtvf');
