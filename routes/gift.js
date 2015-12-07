b47da40a39005ca65c98039d3b6d40007e12386d
Curtis <curtis99877@gmail.com>
2015-12-07 03:04:12 -0500

    Added Ebay API

--- routes/gift.js
+++ routes/gift.js
@@ -1,16 +1,68 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User');
+var List = mongoose.model('List');
+var Gift = mongoose.model('Gift');
 
 router.get('/', function(req, res, next) {
-  res.redirect('/');
+  if (!req.user) res.redirect('/login');
+  else {
+    User.findOne({ 'username': req.user.username }, function (err, user) {
+      if (user.giftList) {
+        List.findOne({ '_id': user.giftlist }, function (err, giftList) {
+          var idList = giftList.gifts.map(function (ele) {
+            return ele.gift;
+          });
+          Gift.find({ '_id': { $in: idlist } }, function (err, gifts) {
+            res.render('gift/gift', {
+              title: "My Gifts",
+              page_title: "My Gifts",
+              gifts: gifts
+            });
+          });
+        });
+      } else {
+        var list = new List({
+          list_name: "Public List",
+          list_owner: user._id,
+          gifts: []
+        });
+        list.save(function() {
+          res.render('gift/gift', {
+            title: "My Gifts",
+            page_title: "My Gifts",
+            gifts: []
+          });
+        });
+      }
+    });
+  }
 });
 
 router.get('/add', function(req, res, next) {
-  res.render('add_gift');
+  if (!req.user) res.redirect('/login');
+  else {
+    res.render('gift/gift_add', {
+      title: "Add Gift",
+      page_title: "Add Gift"
+    });
+  }
 });
 
-router.get('/search', function(req, res, next) {
-  res.render('search');
+router.get('/info', function(req, res, next) {
+  if (!req.query.id) res.redirect('/');
+  else {
+    Gift.findOne({ '_id': req.query.id }, function (err, gift) {
+      // TODO: Catch err
+      res.render('gift/gift_info', {
+        title: "Gift Information",
+        page_title: "Gift Information",
+        gift: gift
+      });
+    });
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -14,51 +14,6 @@ router.get('/', function(req, res, next) {
   }
 });
 
-router.get('/gifts', function(req, res, next) {
-  if (!req.user) res.redirect('/login');
-  else {
-    User.findOne({ 'username': req.user.username }, function (err, user) {
-      if (user.giftList) {
-        List.findOne({ '_id': user.giftlist }, function (err, giftList) {
-          var idList = giftList.gifts.map(function (ele) {
-            return ele.gift;
-          });
-          Gift.find({ '_id': { $in: idlist } }, function (err, gifts) {
-            res.render('user/gift', {
-              title: "My Gifts",
-              page_title: "My Gifts",
-              gifts: gifts
-            });
-          });
-        });
-      } else {
-        var list = new List({
-          list_name: "Public List",
-          list_owner: user._id,
-          gifts: []
-        });
-        list.save(function() {
-          res.render('user/gift', {
-            title: "My Gifts",
-            page_title: "My Gifts",
-            gifts: []
-          });
-        });
-      }
-    });
-  }
-});
-
-router.get('/gifts/add', function(req, res, next) {
-  if (!req.user) res.redirect('/login');
-  else {
-    res.render('user/gift_add', {
-      title: "Add Gift",
-      page_title: "Add Gift"
-    });
-  }
-});
-
 router.get('/:username', function(req, res, next) {
   User.findOne({'username': req.params.username}, function(err, user) {
     if (err) next();
