e6ab832ab3b61ef3cdcbe376da46fce8b3a54dba
Curtis <curtis99877@gmail.com>
2015-12-08 23:09:11 -0500

    Remove events

--- public/javascript/list.js
+++ public/javascript/list.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
   var giftList = document.getElementById('gift_list');
-  var links = document.getElementsByClassName('rem_link');
+  var eventList = document.getElementById('event_list');
+  
+  var giftRemLinks = giftList.getElementsByClassName('rem_link');
+  var eventRemLinks = eventList.getElementsByClassName('rem_link');
 
-  Array.prototype.forEach.call(links, function (link) {
+  Array.prototype.forEach.call(giftRemLinks, function (link) {
     link.addEventListener('click', removeGift);
+  });
+
+  Array.prototype.forEach.call(eventRemLinks, function (link) {
+    link.addEventListener('click', removeEvent);
   });
 
   function removeGift (evt) {
@@ -18,17 +25,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
     req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
     req.send('id=' + target.id);
    
-    removeRow(row);
+    removeRow(row, giftList);
     return false;
   }
 
-  function removeRow (row) {
-    var table = giftList.getElementsByTagName('tbody')[0];
-    console.log(table);
-    console.log(row);
+  function removeEvent (evt) {
+    evt.preventDefault();
+
+    var target = evt.target || evt.srcElement;
+    var row = target.parentNode.parentNode;
+
+    var req = new XMLHttpRequest();
+    req.open('POST', '/gift/remove_event', true);
+    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    req.send('id=' + target.id);
+   
+    removeRow(row, eventList);
+    return false;
+  }
+
+  function removeRow (row, list) {
+    var table = list.getElementsByTagName('tbody')[0];
     table.removeChild(row);
 
     if (table.children.length == 1)
-      giftList.innerHTML = "<p>No gifts in your list.</p>";
+      list.innerHTML = "<p>Nothing in your list.</p>";
   }
 });
